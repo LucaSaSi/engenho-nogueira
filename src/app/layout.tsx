b1a9df2af086b25609930445323f8b3bed82d82f
LucasSSLV <lukinhas.kell@hotmail.com>
2025-05-08 18:20:00 -0300

    style: import fonts from goolge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Sigmar_One, Agbalumo, Rubik_Doodle_Shadow } from "next/font/google";
+
+const rubikDoodleShadow = Rubik_Doodle_Shadow({
+  variable: "--font-rubik-doodle-shadow",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const sigmar = Sigmar_One({
+  variable: "--font-sigmar",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const agbalumo = Agbalumo({
+  variable: "--font-agbalumo",
+  subsets: ["latin"],
+  weight: "400",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +42,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" >
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`
+          ${geistSans.variable}
+         ${geistMono.variable}
+          ${sigmar.variable}
+           ${agbalumo.variable} 
+           ${rubikDoodleShadow.variable}
+           antialiased`}
       >
         {children}
       </body>
-    </html>
+    </html >
   );
 }
