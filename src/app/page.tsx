48796556aef95aef99867f28f43f33dfc88fe6e4
LucasSSLV <lukinhas.kell@hotmail.com>
2025-05-24 12:11:25 -0300

    style: there are styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           <h1
             className="inset-0 pl-3 h-1/2 w-full flex items-center text-3xl md:text-6xl text-gray-200 font-bold text-left"
             style={{
-              fontFamily: "var(--font-rubik-doodle-shadow)",
+              fontFamily: "var(--font-joti-one)",
               fontSize: "4rem",
               textShadow: "4px 4px 8px rgba(0, 0, 0, 0.7)",
             }}
@@ -39,17 +39,17 @@ export default function Home() {
 
       {/* Introdução */}
       <section className="p-6 md:p-12 max-w-4xl mx-auto text-center">
-        <h2 className="text-2xl md:text-3xl font-semibold mb-4 font-">
+        <h2 className="text-2xl md:text-3xl font-semibold mb-4 font-" style={{ fontFamily: "MinhaFonte" }}>
           Tradição, sabor e história desde 1892
         </h2>
-        <p className="text-lg leading-relaxed">
+        <p className="text-lg leading-relaxed" style={{ fontFamily: "var(--font-dosis)", fontSize: 20 }}>
           O Engenho está localizado no Vale do Lambedouro, zona rural do município de Viçosa do Ceará, tendo uma distância aproximada de 15 km do centro da cidade até o mesmo. O percurso é feito boa parte em asfalto, contando apenas com 2,5 km de estrada de terra, mas sendo possível o tráfego de vans e micro-ônibus durante todo o ano.
           No trajeto o visitante pode apreciar a paisagem natural e singular da região, enquanto desce a ladeira que liga o centro à zona rural.        </p>
       </section>
 
       {/* Sobre o Engenho */}
-      <section id="sobre" className="p-6 md:p-12 bg-gray-100">
-        <h2 className="text-2xl font-bold mb-4 text-center">Sobre o Engenho</h2>
+      <section id="sobre" className="p-6 md:p-12 bg-gray-100 border-t-2 border-gray-200">
+        <h2 className="text-3xl font-bold mb-4 text-center" style={{ fontFamily: "MinhaFonte" }}>Casa Grande</h2>
         <div className="max-w-4xl mx-auto text-center text-gray-800 space-y-4">
           <Image
             src="/casa-grande.png"
@@ -57,7 +57,7 @@ export default function Home() {
             className="rounded-lg mb-4"
             width={800}
             height={400} />
-          <p>Desde os meados do século XX, a família Nogueira mantém a tradição de produzir cachaça artesanal de alambique e de cuidar da história singular desta produção de aguardente.
+          <p style={{ fontFamily: "var(--font-borel)", fontSize: 23 }}>Desde os meados do século XX, a família Nogueira mantém a tradição de produzir cachaça artesanal de alambique e de cuidar da história singular desta produção de aguardente.
             O processo de produção artesanal, a vivência da cultura sertaneja e os sabores da culinária tradicional do sertão são atrativos que proporcionam aos visitantes momentos inesquecíveis.
             A casa grande, como é conhecida pela família, é o contato com um pedaço da história de Viçosa do Ceará, contada através da arquitetura, culinária e agricultura familiar mantida há 111 anos.</p>
         </div>
@@ -70,7 +70,7 @@ export default function Home() {
         <div className="grid gap-7 md:grid-cols-2 lg:grid-cols-3">
           {/* Produto 1 */}
           <div
-            style={{ fontFamily: "MinhaFonte" }}
+            style={{ fontFamily: "var(--font-radley)" }}
             className="bg-white rounded-xl shadow-md p-4 text-center hover:scale-105 transition-transform duration-300">
             <Image
               src="/produto1.png"
@@ -79,12 +79,12 @@ export default function Home() {
               width={300}
               height={300} />
             <h3 className="text-lg font-semibold" >LICOR DE BOMBOM</h3>
-            <p className="text-sm text-gray-600">Licor de Bombom: A mistura de sabores da cachaça Nogueira e os toques doces dos bombons de morango, menta, café e chocolate</p>
+            <p className="text-sm text-gray-700" style={{ fontFamily: "var(--font-dosis)" }}>Licor de Bombom: A mistura de sabores da cachaça Nogueira e os toques doces dos bombons de morango, menta, café e chocolate</p>
           </div>
 
           {/* Produto 2 */}
           <div
-            style={{ fontFamily: "MinhaFonte" }}
+            style={{ fontFamily: "var(--font-radley)" }}
             className="bg-white rounded-xl shadow-md p-4 text-center hover:scale-105 transition-transform duration-300">
             <Image
               src="/produto2.png"
@@ -93,12 +93,12 @@ export default function Home() {
               width={400}
               height={400} />
             <h3 className="text-lg font-semibold mt-2">CACHAÇA ENVELHECIDA</h3>
-            <p className="text-sm text-gray-600">Cachaças envelhecidas: Envelhecidas em toneis de Aroeira, Umburana, Bálsamo, Carvalho Europeu, Pau D'arco, Sabiá e Jaqueira. Classificadas em Premium, Extra Premium e Reserva Especial</p>
+            <p className="text-sm text-gray-700" style={{ fontFamily: "var(--font-dosis)" }}>Cachaças envelhecidas: Envelhecidas em toneis de Aroeira, Umburana, Bálsamo, Carvalho Europeu, Pau D'arco, Sabiá e Jaqueira. Classificadas em Premium, Extra Premium e Reserva Especial</p>
           </div>
 
           {/* Produto 3 */}
           <div
-            style={{ fontFamily: "MinhaFonte" }}
+            style={{ fontFamily: "var(--font-radley)" }}
             className="bg-white rounded-xl shadow-md p-4 text-center hover:scale-105 transition-transform duration-300">
             <Image
               src="/produto3.png"
@@ -107,43 +107,49 @@ export default function Home() {
               width={400}
               height={400} />
             <h3 className="text-lg font-semibold mt-2">CACHAÇA PRATA</h3>
-            <p className="text-sm text-gray-600">Cachaça Prata: O sabor autêntico da cachaça de alambique de cobre produzida em Viçosa do Ceará</p>
+            <p className="text-sm text-gray-700" style={{ fontFamily: "var(--font-dosis)" }}>Cachaça Prata: O sabor autêntico da cachaça de alambique de cobre produzida em Viçosa do Ceará</p>
           </div>
 
           {/* Produto 4 */}
-          <div className="bg-white rounded-xl shadow-md p-4 text-center hover:scale-105 transition-transform duration-300">
+          <div
+            style={{ fontFamily: "var(--font-radley)" }}
+            className="bg-white rounded-xl shadow-md p-4 text-center hover:scale-105 transition-transform duration-300">
             <Image
               src="/produto4.png"
-              alt="Licor artesanal"
+              alt="SUVENIR"
               className="h-100 w-full mx-auto object-cover rounded-lg"
               width={400}
               height={400} />
-            <h3 className="text-lg font-semibold mt-2">Suvenir</h3>
-            <p className="text-sm text-gray-600">Souvernir: Produtos que levam a marca e o slogam da cachaça Nogueira. Como bonés, copos e camisas.</p>
+            <h3 className="text-lg font-semibold mt-2">SUVENIR</h3>
+            <p className="text-sm text-gray-700" style={{ fontFamily: "var(--font-dosis)" }}>Souvernir: Produtos que levam a marca e o slogam da cachaça Nogueira. Como bonés, copos e camisas.</p>
           </div>
 
           {/* Produto 5 */}
-          <div className="bg-white rounded-xl shadow-md p-4 text-center hover:scale-105 transition-transform duration-300">
+          <div
+            style={{ fontFamily: "var(--font-radley)" }}
+            className="bg-white rounded-xl shadow-md p-4 text-center hover:scale-105 transition-transform duration-300">
             <Image
               src="/produto5.png"
-              alt="Licor artesanal"
+              alt="LICOR TRADICIONAL"
               className="h-100 w-full mx-auto object-cover rounded-lg"
               width={400}
               height={400} />
-            <h3 className="text-lg font-semibold mt-2">Licor Tradicional</h3>
-            <p className="text-sm text-gray-600">Licores Tradicionais: A mistura de sabores da Cachaça Nogueira e das especiarias, raízes, folhas e frutos tradicionais da região.</p>
+            <h3 className="text-lg font-semibold mt-2">LICOR TRADICIONAL</h3>
+            <p className="text-sm text-gray-700" style={{ fontFamily: "var(--font-dosis)" }}>Licores Tradicionais: A mistura de sabores da Cachaça Nogueira e das especiarias, raízes, folhas e frutos tradicionais da região.</p>
           </div>
 
           {/* Produto 6 */}
-          <div className="bg-white rounded-xl shadow-md p-4 text-center hover:scale-105 transition-transform duration-300">
+          <div
+            style={{ fontFamily: "var(--font-radley)" }}
+            className="bg-white rounded-xl shadow-md p-4 text-center hover:scale-105 transition-transform duration-300">
             <Image
               src="/produto6.png"
-              alt="Licor artesanal"
+              alt="BIBIDA ALCÓOLICA MISTA"
               className="h-100 w-full mx-auto object-cover rounded-lg"
               width={400}
               height={400} />
-            <h3 className="text-lg font-semibold mt-2">Bebida alcólica mista</h3>
-            <p className="text-sm text-gray-600">Bebida: Uma caipirinha pronta que vai surpreender com o sabor das frutas e da cachaça Nogueira, ideal para tomar gelada ou preparar seu drink.</p>
+            <h3 className="text-lg font-semibold mt-2">BEBIDA ALCÓOLICA MISTA</h3>
+            <p className="text-sm text-gray-700" style={{ fontFamily: "var(--font-dosis)" }}>Bebida: Uma caipirinha pronta que vai surpreender com o sabor das frutas e da cachaça Nogueira, ideal para tomar gelada ou preparar seu drink.</p>
           </div>
         </div>
 
