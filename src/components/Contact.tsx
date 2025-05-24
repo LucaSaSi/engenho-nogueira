9e8690b1dd505b82ebd94cb49c3e875c2741eefd
LucasSSLV <lukinhas.kell@hotmail.com>
2025-05-24 15:00:14 -0300

    feat: organizing into components

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
     return (
         <section id="contato" className="bg-white py-16 px-4">
             <div className="max-w-4xl mx-auto text-center">
-                <h2 className="text-2xl md:text-3xl font-bold mb-6">Entre em Contato</h2>
+                <h2 className="text-2xl md:text-3xl font-bold mb-6">Contato e mais</h2>
                 <p className="text-gray-700 mb-6">Fale conosco ou acompanhe nas redes sociais:</p>
 
                 <div className="flex justify-center flex-wrap gap-6">
