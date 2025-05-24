bb9dcc62e458b192d41ab98bd55067aa878bfbdb
LucasSSLV <lukinhas.kell@hotmail.com>
2025-05-24 12:11:43 -0300

    feat: there are styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,19 +8,19 @@ export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <header className="bg-green-100 shadow-md fixed top-0 w-full z-50">
+        <header className="flex bg-gray-800 shadow-md fixed top-0 w-full h-20 z-50">
             <div
                 style={{
                     fontFamily: 'var(--font-sigmar)',
                 }}
-                className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center ">
+                className="w-full px-4 py-3 flex justify-between items-center  ">
                 {/* Logo */}
                 <Image
                     src="/logo.png"
                     alt="logo engenho nogueira"
                     width={200}
                     height={200}
-                    className="rounded-full w-40 h-20 md:w-50 md:h-25"
+                    className="rounded-full w-40 h-20 md:w-50 md:h-25 "
                 />
 
                 {/* Botão do menu para dispositivos móveis */}
@@ -47,26 +47,27 @@ export default function Header() {
                 {/* Navegação */}
                 <nav
                     className={`${isMenuOpen ? 'block' : 'hidden'
-                        } absolute top-full left-0 w-full bg-green-100 md:static md:flex md:space-x-4 md:w-auto `}
+                        } absolute top-full left-0 w-80  bg-gray-800 md:static md:flex md:space-x-4 md:w-auto `}
                 >
-                    <a
+                    <Link
+                        style={{ fontFamily: "var(--font-radley)" }}
                         target="_blank"
                         rel="noopener noreferrer"
                         href="https://www.google.com/maps/dir//Lambedouro,+Vi%C3%A7osa+do+Cear%C3%A1+-+CE,+62300-000+@-3.581498,-41.022260/@-3.5818372,-41.0221632,17z/data=!4m7!4m6!1m1!4e2!1m2!1m1!1s0x7eb41bdc35ffe85:0xcde5000c89462cfe!3e0"
-                        className="block text-2xl text-gray-800 hover:text-green-700 transition py-2 px-4 md:inline"
+                        className="block text-2xl text-gray-100 hover:text-green-700 transition py-2 px-4 md:inline"
                     >
                         Localização
-                    </a>
-                    <Link href="#sobre" className="block text-2xl text-gray-800 hover:text-green-700 transition py-2 px-4 md:inline">
+                    </Link>
+                    <Link style={{ fontFamily: "var(--font-radley)" }} href="#sobre" className="block text-2xl text-gray-100 hover:text-green-700 transition py-2 px-4 md:inline">
                         Sobre
                     </Link>
-                    <Link href="#produtos" className="block text-2xl text-gray-800 hover:text-green-700 transition py-2 px-4 md:inline">
+                    <Link style={{ fontFamily: "var(--font-radley)" }} href="#produtos" className="block text-2xl text-gray-100 hover:text-green-700 transition py-2 px-4 md:inline">
                         Produtos
                     </Link>
-                    <Link href="#visitas" className="block text-2xl text-gray-800 hover:text-green-700 transition py-2 px-4 md:inline">
+                    <Link style={{ fontFamily: "var(--font-radley)" }} href="#visitas" className="block text-2xl text-gray-100 hover:text-green-700 transition py-2 px-4 md:inline">
                         Visitas
                     </Link>
-                    <Link href="#contato" className="block text-2xl text-gray-800 hover:text-green-700 transition py-2 px-4 md:inline">
+                    <Link style={{ fontFamily: "var(--font-radley)" }} href="#contato" className="block text-2xl text-gray-100 hover:text-green-700 transition py-2 px-4 md:inline">
                         Contato
                     </Link>
                 </nav>
