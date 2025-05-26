2342efc5be3e44984965fee8c5224b2ac1188e37
LucasSSLV <lukinhas.kell@hotmail.com>
2025-05-26 12:23:34 -0300

    style: add styling

--- src/app/Restaurant/page.tsx
+++ src/app/Restaurant/page.tsx
@@ -5,9 +5,9 @@ import React from 'react';
 export default function RestaurantPage() {
 
   return (
-    <main className="max-full mx-auto p-4 text-gray-900 bg-emerald-800">
+    <main className="max-full mx-auto p-4 text-gray-900 bg-gradient-to-b from-emerald-800 to-emerald-200 min-h-screen">
       {/* <DraggableBox /> */}
-      <h1 className="md:text-4xl text-3xl font-bold text-center mb-6" style={{ fontFamily: "var(--font-radley)" }}>
+      <h1 className="md:text-5xl text-3xl text-teal-100 font-bold text-center mb-6" style={{ fontFamily: "var(--font-joti-one)" }}>
         Cardápio do Restaurante
       </h1>
 
@@ -21,54 +21,54 @@ export default function RestaurantPage() {
 
           {/* Entradas */}
           <section className="mb-6 border-b-2 border-gray-300 p-4 rounded-lg">
-            <h2 className="text-2xl text-center font-semibold mb-2 border-b-2" >Entradas</h2>
+            <h2 className="text-2xl md:text-gray-900 text-teal-50 text-center font-semibold mb-2 border-b-2 border-gray-900" >Entradas</h2>
             <ul className="space-y-1">
-              <li className="flex justify-between"><span>Calabresa Flambada</span><span>R$25,00</span></li>
-              <li className="flex justify-between"><span>Macaxeira frita</span><span>R$20,00</span></li>
-              <li className="flex justify-between"><span>Batata frita</span><span>R$15,00</span></li>
-              <li className="flex justify-between"><span>Ovo de codorna</span><span>R$15,00</span></li>
-              <li className="flex justify-between"><span>Queijo com orégano</span><span>R$17,00</span></li>
-              <li className="flex justify-between"><span>Queijo com mel</span><span>R$17,00</span></li>
-              <li className="flex justify-between"><span>Paçoca</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Calabresa Flambada</span><span className='flex flex-col text-amber-950 font-semibold'>R$25,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Macaxeira frita</span><span className='flex flex-col text-amber-950 font-semibold'>R$20,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Batata frita</span><span className='flex flex-col text-amber-950 font-semibold'>R$15,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Ovo de codorna</span><span className='flex flex-col text-amber-950 font-semibold'>R$15,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Queijo com orégano</span><span className='flex flex-col text-amber-950 font-semibold'>R$17,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Queijo com mel</span><span className='flex flex-col text-amber-950 font-semibold'>R$17,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Paçoca</span></li>
             </ul>
           </section>
 
           {/* Guarnições */}
           <section className="mb-6 border-b-2 border-gray-300 p-4 rounded-lg">
-            <h2 className="text-2xl text-center font-semibold mb-2 border-b-2">Guarnições</h2>
+            <h2 className="text-2xl md:text-gray-900 text-teal-50 text-center font-semibold mb-2 border-b-2 border-gray-900">Guarnições</h2>
             <ul className="space-y-1">
-              <li className="flex justify-between"><span>Arroz</span><span>R$12,00</span></li>
-              <li className="flex justify-between"><span>Feijão</span><span>R$12,00</span></li>
-              <li className="flex justify-between"><span>Fava</span><span>R$12,00</span></li>
-              <li className="flex justify-between"><span>Farofa</span><span>R$5,00</span></li>
-              <li className="flex justify-between"><span>Salada</span><span>R$5,00</span></li>
-              <li className="flex justify-between"><span>Pirão</span><span>R$10,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Arroz</span><span className='flex flex-col text-amber-950 font-semibold'>R$12,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Feijão</span><span className='flex flex-col text-amber-950 font-semibold'>R$12,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Fava</span><span className='flex flex-col text-amber-950 font-semibold'>R$12,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Farofa</span><span className='flex flex-col text-amber-950 font-semibold'>R$5,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Salada</span><span className='flex flex-col text-amber-950 font-semibold'>R$5,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Pirão</span><span className='flex flex-col text-amber-950 font-semibold'>R$10,00</span></li>
             </ul>
           </section>
 
           {/* Refeições */}
           <section className="mb-6 border-b-2 border-gray-300 p-4 rounded-lg">
-            <h2 className="text-2xl text-center font-semibold mb-2 border-b-2">Refeições</h2>
+            <h2 className="text-2xl md:text-gray-900 text-teal-50 text-center font-semibold mb-2 border-b-2 border-gray-900">Refeições</h2>
             <ul className="space-y-2">
               <li>
-                <span className="font-semibold underline">Galinha Caipira</span>
-                <span className="block text-sm font-bold text-gray-800">Arroz, feijão ou fava, pirão, salada e farofa</span>
-                <span className="block">4 Pessoas: <b>R$140,00</b> | 2 Pessoas: <b>R$80,00</b></span>
+                <span className="text-2xl md:text-gray-900 text-teal-400 text-center font-semibold mb-2 border-b-2 border-gray-900">Galinha Caipira</span>
+                <span className="block text-sm text-amber-950 font-semibold">Arroz, feijão ou fava, pirão, salada e farofa</span>
+                <span className=" flex flex-col text-amber-950 font-semibold">4 Pessoas: R$140,00 <b></b> 2 Pessoas: R$80,00</span>
               </li>
               <li>
-                <span className="font-semibold underline">Pato à Cabidela</span>
-                <span className="block text-sm font-bold text-gray-800">Arroz, feijão ou fava, farofa e salada</span>
-                <span className="block">4 Pessoas: <b>R$150,00</b> | 2 Pessoas: <b>R$85,00</b></span>
+                <span className="text-2xl md:text-gray-900 text-teal-400 text-center font-semibold mb-2 border-b-2 border-gray-900">Pato à Cabidela</span>
+                <span className="block text-sm text-amber-950 font-semibold">Arroz, feijão ou fava, farofa e salada</span>
+                <span className=" flex flex-col text-amber-950 font-semibold">4 Pessoas: R$150,00 <b></b> 2 Pessoas: R$85,00</span>
               </li>
               <li>
-                <span className="font-semibold underline">Porco Assado</span>
-                <span className="block text-sm font-bold text-gray-800">Arroz, feijão ou fava, farofa e salada</span>
-                <span className="block">4 Pessoas: <b>R$130,00</b> | 2 Pessoas: <b>R$75,00</b></span>
+                <span className="text-2xl md:text-gray-900 text-teal-400 text-center font-semibold mb-2 border-b-2 border-gray-900">Porco Assado</span>
+                <span className="block text-sm text-amber-950 font-semibold">Arroz, feijão ou fava, farofa e salada</span>
+                <span className=" flex flex-col text-amber-950 font-semibold">4 Pessoas: R$130,00 <b></b> 2 Pessoas: R$75,00</span>
               </li>
               <li>
-                <span className="font-semibold underline">Assado de Panela</span>
-                <span className="block text-sm font-bold text-gray-800">Arroz, feijão ou fava, farofa e salada</span>
-                <span className="block">4 Pessoas: <b>R$130,00</b> | 2 Pessoas: <b>R$75,00</b></span>
+                <span className="text-2xl md:text-gray-900 text-teal-400 text-center font-semibold mb-2 border-b-2 border-gray-900">Assado de Panela</span>
+                <span className="block text-sm text-amber-950 font-semibold">Arroz, feijão ou fava, farofa e salada</span>
+                <span className=" flex flex-col text-amber-950 font-semibold">4 Pessoas: R$130,00 <b></b> 2 Pessoas: R$75,00</span>
               </li>
             </ul>
           </section>
@@ -77,45 +77,45 @@ export default function RestaurantPage() {
       </div>
 
       <div className='max-w-3xl mx-auto bg-white p-2 rounded-lg shadow-lg mt-8'
-        style={{ backgroundImage: "url('/brinde.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}
+        style={{ backgroundImage: "url('/brinde.png')", backgroundSize: 'contain', backgroundPosition: 'center' }}
       >
         <div className='p-4 rounded-lg bg-white/30 backdrop-blur-sm'>
 
           {/* Bebidas */}
           <section className="mb-6 border-b-2 border-gray-300 p-4 rounded-lg">
-            <h2 className="text-2xl text-center font-semibold mb-2 border-b-2">Bebidas</h2>
+            <h2 className="text-2xl md:text-gray-900 text-teal-50 text-center font-semibold mb-2 border-b-2 border-gray-900">Bebidas</h2>
             <ul className="space-y-1">
-              <li className="flex justify-between"><span>Água Mineral</span><span>R$3,00</span></li>
-              <li className="flex justify-between"><span>Água com gás</span><span>R$4,00</span></li>
-              <li className="flex justify-between"><span>Refrigerante (lata)</span><span>R$6,00</span></li>
-              <li className="flex justify-between"><span>Refrigerante (1l)</span><span>R$10,00</span></li>
-              <li className="flex justify-between"><span>Jarra de Suco</span><span>R$12,00</span></li>
-              <li className="flex justify-between"><span>Suco (copo)</span><span>R$6,00</span></li>
-              <li className="flex justify-between"><span>Heineken (330ml)</span><span>R$12,00</span></li>
-              <li className="flex justify-between"><span>Brahma (350ml)</span><span>R$7,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Água Mineral</span><span className='flex flex-col text-amber-950 font-semibold'>R$3,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Água com gás</span><span className='flex flex-col text-amber-950 font-semibold'>R$4,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Refrigerante (lata)</span><span className='flex flex-col text-amber-950 font-semibold'>R$6,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Refrigerante (1l)</span><span className='flex flex-col text-amber-950 font-semibold'>R$10,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Jarra de Suco</span><span className='flex flex-col text-amber-950 font-semibold'>R$12,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Suco (copo)</span><span className='flex flex-col text-amber-950 font-semibold'>R$6,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Heineken (330ml)</span><span className='flex flex-col text-amber-950 font-semibold'>R$12,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Brahma (350ml)</span><span className='flex flex-col text-amber-950 font-semibold'>R$7,00</span></li>
             </ul>
           </section>
 
           {/* Drinks */}
           <section className="mb-6 border-b-2 border-gray-300 p-4 rounded-lg">
-            <h2 className="text-2xl text-center font-semibold mb-2 border-b-2">Drinks</h2>
+            <h2 className="text-2xl md:text-gray-900 text-teal-50 text-center font-semibold mb-2 border-b-2 border-gray-900">Drinks</h2>
             <ul className="space-y-1">
-              <li className="flex justify-between"><span>Caipirinha Tradicional</span><span>R$10,00</span></li>
-              <li className="flex justify-between"><span>Batida Cremosa</span><span>R$12,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Caipirinha Tradicional</span><span className='flex flex-col text-amber-950 font-semibold'>R$10,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Batida Cremosa</span><span className='flex flex-col text-amber-950 font-semibold'>R$12,00</span></li>
             </ul>
           </section>
 
           {/* Sobremesa */}
           <section className="mb-6 border-b-2 border-gray-300 p-4 rounded-lg">
-            <h2 className="text-2xl text-center font-semibold mb-2 border-b-2">Sobremesa</h2>
+            <h2 className="text-2xl md:text-gray-900 text-teal-50 text-center font-semibold mb-2 border-b-2 border-gray-900">Sobremesa</h2>
             <ul className="space-y-1">
-              <li className="flex justify-between"><span>Doce (porção individual)</span><span>R$7,00</span></li>
+              <li className="flex justify-between md:text-gray-800 text-teal-50 font-semibold"><span>Doce (porção individual)</span><span className='flex flex-col text-amber-950 font-semibold'>R$7,00</span></li>
             </ul>
-            <p className="text-sm text-gray-600 mt-1">** Consultar as opções disponíveis. **</p>
+            <p className="text-sm text-gray-100 mt-1">** Consultar as opções disponíveis. **</p>
           </section>
         </div>
       </div>
-      <p className="text-center text-sm text-gray-200 mt-4">Obs: Cobramos taxa de 10%</p>
+      <p className="text-center text-sm text-gray-800 mt-4">Obs: Cobramos taxa de 10%</p>
     </main>
   );
 }
